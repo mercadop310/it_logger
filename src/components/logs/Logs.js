c9eb8a672627badd129ea66ca89d3d582b6ca2ca
Peter Mercado <peter@Peters-MBP.lan>
2020-05-21 11:21:37 -0700

    finished first step of log component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import LogItem from '../../components/logs/LogItem';
+import Preloader from '../layout/Preloader';
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -18,7 +20,7 @@ const Logs = () => {
     setLoading(false);
   };
   if (loading) {
-    return <h4>Loading...</h4>;
+    return <Preloader />;
   }
   return (
     <ul className='collection with-header'>
@@ -28,7 +30,7 @@ const Logs = () => {
       {!loading && logs.length === 0 ? (
         <p className='center'>No logs to show</p>
       ) : (
-        logs.map((log) => <li>{log.message}</li>)
+        logs.map((log) => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   );
